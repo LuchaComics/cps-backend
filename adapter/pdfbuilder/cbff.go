fcbe836ce055e477323ee897b6a9d8048c076de5
Bartlomiej Mika <bmika@icloud.com>
2023-05-16 01:33:04 -0400

    Latest progress with PDF generator

--- adapter/pdfbuilder/cbff.go
+++ adapter/pdfbuilder/cbff.go
@@ -6,7 +6,9 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"time"
 
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"golang.org/x/exp/slog"
 
 	c "github.com/LuchaComics/cps-backend/config"
@@ -16,11 +18,46 @@ import (
 )
 
 type CBFFBuilderRequestDTO struct {
-	Id        string `json:"id"`
-	Uuid      string `json:"uuid"`
-	TenantID  string `json:"tenant_id"`
-	OrderId   string `json:"order_id"`
-	InvoiceID string `json:"invoice_id"`
+	ID                       primitive.ObjectID `bson:"_id" json:"id"`
+	Date                     time.Time          `bson:"date" json:"date"`
+	Item                     string             `bson:"item" json:"item"` // Created by system.
+	SeriesTitle              string             `bson:"series_title" json:"series_title"`
+	IssueVol                 string             `bson:"issue_vol" json:"issue_vol"`
+	IssueNo                  string             `bson:"issue_no" json:"issue_no"`
+	IssueCoverDate           string             `bson:"issue_cover_date" json:"issue_cover_date"`
+	PublisherName            string             `bson:"publisher_name" json:"publisher_name"`
+	IssueSpecialDetails      string             `bson:"issue_special_details" json:"issue_special_details"`
+	CreasesFinding           string             `bson:"creases_finding" json:"creases_finding"`
+	TearsFinding             string             `bson:"tears_finding" json:"tears_finding"`
+	MissingPartsFinding      string             `bson:"missing_parts_finding" json:"missing_parts_finding"`
+	StainsFinding            string             `bson:"stains_finding" json:"stains_finding"`
+	DistortionFinding        string             `bson:"distortion_finding" json:"distortion_finding"`
+	PaperQualityFinding      string             `bson:"paper_quality_finding" json:"paper_quality_finding"`
+	SpineFinding             string             `bson:"spine_finding" json:"spine_finding"`
+	CoverFinding             string             `bson:"cover_finding" json:"cover_finding"`
+	OtherFinding             string             `bson:"other_finding" json:"other_finding"`
+	OtherFindingText         string             `bson:"other_finding_text" json:"other_finding_text"`
+	OverallLetterGrade       string             `bson:"overall_letter_grade" json:"overall_letter_grade"`
+	UserID                   primitive.ObjectID `bson:"user_id" json:"user_id"`
+	UserFirstName            string             `bson:"user_first_name" json:"user_first_name"`
+	UserLastName             string             `bson:"user_last_name" json:"user_last_name"`
+	UserCompanyName          string             `bson:"user_company_name" json:"user_company_name"`
+	UserSignature            string             `bson:"user_signature" json:"user_signature"`
+	InspectorSignature       string             `bson:"inspector_signature" json:"inspector_signature"`
+	InspectorDate            time.Time          `bson:"inspector_date" json:"inspector_date"`
+	InspectorFirstName       string             `bson:"inspector_first_name" json:"inspector_first_name"`
+	InspectorLastName        string             `bson:"inspector_last_name" json:"inspector_last_name"`
+	InspectorCompany         string             `bson:"inspector_company_name" json:"inspector_company_name"`
+	SecondInspectorSignature string             `bson:"second_inspector_signature" json:"second_inspector_signature"`
+	SecondInspectorFirstName string             `bson:"second_inspector_first_name" json:"second_inspector_first_name"`
+	SecondInspectorLastName  string             `bson:"second_inspector_last_name" json:"second_inspector_last_name"`
+	SecondInspectorCompany   string             `bson:"second_inspector_company" json:"second_inspector_company"`
+	SecondInspectorDate      time.Time          `bson:"second_inspector_date" json:"second_inspector_date"`
+	ThirdInspectorSignature  string             `bson:"third_inspector_signature" json:"third_inspector_signature"`
+	ThirdInspectorFirstName  string             `bson:"third_inspector_first_name" json:"third_inspector_first_name"`
+	ThirdInspectorLastName   string             `bson:"third_inspector_last_name" json:"third_inspector_last_name"`
+	ThirdInspectorCompany    string             `bson:"third_inspector_company" json:"third_inspector_company"`
+	ThirdInspectorDate       time.Time          `bson:"third_inspector_date" json:"third_inspector_date"`
 }
 type CBFFBuilderResponseDTO struct {
 	FileName string `json:"file_name"`
@@ -64,459 +101,193 @@ func (bdr *cbffBuilder) GeneratePDF(r *CBFFBuilderRequestDTO) (*CBFFBuilderRespo
 	pdf.AddPage()
 
 	// Draw imported template onto page
-	gofpdi.UseImportedTemplate(pdf, tpl1, 0, 0, 297, 0)
+	gofpdi.UseImportedTemplate(pdf, tpl1, 0, 0, 297, 210)
 
-	pdf.SetFont("Courier", "", 11)
+	//
+	// UNIQUE IDENTIFIER
+	//
 
-	pdf.SetXY(159, 19)
-	// pdf.Cell(0, 0, dto.InvoiceID)
+	pdf.SetFont("Courier", "", 12)
+	pdf.SetXY(17, 21)
+	pdf.Cell(0, 0, r.ID.Hex())
+
 	//
-	// pdf.SetFont("Helvetica", "", 11)
+	// LEFT SIDE
 	//
-	// //
-	// // Header
-	// //
+
+	pdf.SetFont("Helvetica", "", 12)
+
+	// ROW 1
+	pdf.SetXY(113, 39)
+	pdf.Cell(0, 0, fmt.Sprintf("%v", r.Date.Day())) // Day
+	pdf.SetXY(126, 39)
+	pdf.Cell(0, 0, fmt.Sprintf("%v", int(r.Date.Month()))) // Month (number)
+	pdf.SetXY(135, 39)
+	pdf.Cell(0, 0, fmt.Sprintf("%v", r.Date.Year())) // Day
+
+	// ROW 2
+	pdf.SetXY(82, 47)
+	pdf.Cell(0, 0, r.UserFirstName)
+	pdf.SetXY(114, 47)
+	pdf.Cell(0, 0, r.UserLastName)
+
+	// ROW 3
+	pdf.SetXY(27, 56)
+	pdf.Cell(0, 0, r.UserCompanyName)
+
 	//
-	// pdf.SetXY(116, 25)
-	// pdf.Cell(0, 0, dto.InvoiceDate)
-	// pdf.SetXY(136, 32)
-	// pdf.Cell(0, 0, dto.AssociateName)
-	// pdf.SetXY(136, 39)
-	// pdf.Cell(0, 0, dto.AssociateTelephone)
-	// pdf.SetXY(46, 46)
-	// pdf.Cell(0, 0, dto.ClientName)
-	// pdf.SetXY(36, 53)
-	// pdf.Cell(0, 0, dto.ClientAddress)
-	// pdf.SetXY(33, 60)
-	// pdf.Cell(0, 0, dto.ClientTelephone)
-	// pdf.SetXY(92, 60)
-	// pdf.Cell(0, 0, dto.ClientEmail)
+	// RIGHT SIDE
 	//
-	// //
-	// // Line #1
-	// //
+
+	// ROW 1
+	pdf.SetXY(162, 39)
+	pdf.Cell(0, 0, r.SeriesTitle)
+
+	// ROW 2
+	pdf.SetXY(160, 47.5)
+	pdf.Cell(0, 0, r.IssueVol)
+	pdf.SetXY(193, 47.5)
+	pdf.Cell(0, 0, r.IssueNo)
+	pdf.SetXY(235, 47.5)
+	pdf.Cell(0, 0, r.IssueCoverDate)
+
+	// ROW 3
+	pdf.SetXY(172, 56)
+	pdf.Cell(0, 0, r.PublisherName)
+
 	//
-	// pdf.SetXY(30, 72)
-	// pdf.Cell(0, 0, dto.Line01Qty)
-	// pdf.SetXY(49, 72)
-	// pdf.Cell(0, 0, dto.Line01Desc)
-	// pdf.SetXY(136, 72)
-	// pdf.Cell(0, 0, dto.Line01Price)
-	// pdf.SetXY(169, 72)
-	// pdf.Cell(0, 0, dto.Line01Amount)
+	// MIDDLE
 	//
-	// //
-	// // Line #2
-	// //
-	//
-	// pdf.SetXY(30, 78)
-	// if dto.Line02Qty == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line02Qty)
-	// }
-	// pdf.SetXY(49, 78)
-	// pdf.Cell(0, 0, dto.Line02Desc)
-	// pdf.SetXY(136, 78)
-	// if dto.Line02Price == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line02Price)
-	// }
-	// pdf.SetXY(169, 78)
-	// if dto.Line02Amount == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line02Amount)
-	// }
-	//
-	// //
-	// // Line #3
-	// //
-	//
-	// pdf.SetXY(30, 83)
-	// if dto.Line03Qty == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line03Qty)
-	// }
-	// pdf.SetXY(49, 83)
-	// pdf.Cell(0, 0, dto.Line03Desc)
-	// pdf.SetXY(136, 83)
-	// if dto.Line03Price == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line03Price)
-	// }
-	// pdf.SetXY(169, 83)
-	// if dto.Line03Amount == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line03Amount)
-	// }
-	//
-	// //
-	// // Line #4
-	// //
-	//
-	// pdf.SetXY(30, 88)
-	// if dto.Line04Qty == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line04Qty)
-	// }
-	// pdf.SetXY(49, 88)
-	// pdf.Cell(0, 0, dto.Line04Desc)
-	// pdf.SetXY(136, 88)
-	// if dto.Line04Price == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line04Price)
-	// }
-	// pdf.SetXY(169, 88)
-	// if dto.Line04Amount == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line04Amount)
-	// }
-	//
-	// //
-	// // Line #5
-	// //
-	//
-	// pdf.SetXY(30, 93)
-	// if dto.Line05Qty == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line05Qty)
-	// }
-	// pdf.SetXY(49, 93)
-	// pdf.Cell(0, 0, dto.Line05Desc)
-	// pdf.SetXY(136, 93)
-	// if dto.Line05Price == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line05Price)
-	// }
-	// pdf.SetXY(169, 93)
-	// if dto.Line05Amount == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line05Amount)
-	// }
-	//
-	// //
-	// // Line #6
-	// //
-	//
-	// pdf.SetXY(30, 98)
-	// if dto.Line06Qty == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line06Qty)
-	// }
-	// pdf.SetXY(49, 98)
-	// pdf.Cell(0, 0, dto.Line06Desc)
-	// pdf.SetXY(136, 98)
-	// if dto.Line06Price == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line06Price)
-	// }
-	// pdf.SetXY(169, 98)
-	// if dto.Line06Amount == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line06Amount)
-	// }
-	//
-	// //
-	// // Line #7
-	// //
-	//
-	// pdf.SetXY(30, 103)
-	// if dto.Line07Qty == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line07Qty)
-	// }
-	// pdf.SetXY(49, 103)
-	// pdf.Cell(0, 0, dto.Line07Desc)
-	// pdf.SetXY(136, 103)
-	// if dto.Line07Price == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line07Price)
-	// }
-	// pdf.SetXY(169, 103)
-	// if dto.Line07Amount == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line07Amount)
-	// }
-	//
-	// //
-	// // Line #8
-	// //
-	//
-	// pdf.SetXY(30, 109)
-	// if dto.Line07Amount == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line07Amount)
-	// }
-	// pdf.SetXY(49, 109)
-	// pdf.Cell(0, 0, dto.Line08Desc)
-	// pdf.SetXY(136, 109)
-	// if dto.Line08Price == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line08Price)
-	// }
-	// pdf.SetXY(169, 109)
-	// if dto.Line08Amount == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line08Amount)
-	// }
-	//
-	// //
-	// // Line #9
-	// //
-	//
-	// pdf.SetXY(30, 114)
-	// if dto.Line09Qty == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line09Qty)
-	// }
-	// pdf.SetXY(49, 114)
-	// pdf.Cell(0, 0, dto.Line09Desc)
-	// pdf.SetXY(136, 114)
-	// if dto.Line09Price == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line09Price)
-	// }
-	// pdf.SetXY(169, 114)
-	// if dto.Line09Amount == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line09Amount)
-	// }
-	//
-	// //
-	// // Line #10
-	// //
-	//
-	// pdf.SetXY(30, 119)
-	// if dto.Line10Qty == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line10Qty)
-	// }
-	// pdf.SetXY(49, 119)
-	// pdf.Cell(0, 0, dto.Line10Desc)
-	// pdf.SetXY(136, 119)
-	// if dto.Line10Price == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line10Price)
-	// }
-	// pdf.SetXY(169, 119)
-	// if dto.Line10Amount == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line10Amount)
-	// }
-	//
-	// //
-	// // Line #11
-	// //
-	//
-	// pdf.SetXY(30, 124)
-	// if dto.Line11Qty == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line11Qty)
-	// }
-	// pdf.SetXY(49, 124)
-	// pdf.Cell(0, 0, dto.Line11Desc)
-	// pdf.SetXY(136, 124)
-	// if dto.Line11Price == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line11Price)
-	// }
-	// pdf.SetXY(169, 124)
-	// if dto.Line11Amount == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line11Amount)
-	// }
-	//
-	// //
-	// // Line #12
-	// //
-	//
-	// pdf.SetXY(30, 129)
-	// if dto.Line12Qty == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line12Qty)
-	// }
-	// pdf.SetXY(49, 129)
-	// pdf.Cell(0, 0, dto.Line12Desc)
-	// pdf.SetXY(136, 129)
-	// if dto.Line12Price == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line12Price)
-	// }
-	// pdf.SetXY(169, 129)
-	// if dto.Line12Amount == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line12Amount)
-	// }
-	//
-	// //
-	// // Line #13
-	// //
-	//
-	// pdf.SetXY(30, 134)
-	// if dto.Line13Qty == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line13Qty)
-	// }
-	// pdf.SetXY(49, 134)
-	// pdf.Cell(0, 0, dto.Line13Desc)
-	// pdf.SetXY(136, 134)
-	// if dto.Line13Price == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line13Price)
-	// }
-	// pdf.SetXY(169, 134)
-	// if dto.Line13Amount == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line13Amount)
-	// }
-	//
-	// //
-	// // Line #14
-	// //
-	//
-	// pdf.SetXY(30, 140)
-	// if dto.Line14Qty == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line14Qty)
-	// }
-	// pdf.SetXY(49, 140)
-	// pdf.Cell(0, 0, dto.Line14Desc)
-	// pdf.SetXY(136, 140)
-	// if dto.Line14Price == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line14Price)
-	// }
-	// pdf.SetXY(169, 140)
-	// if dto.Line14Amount == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line14Amount)
-	// }
-	//
-	// //
-	// // Line #15
-	// //
-	//
-	// pdf.SetXY(30, 145)
-	// if dto.Line15Qty == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line15Qty)
-	// }
-	// pdf.SetXY(49, 145)
-	// pdf.Cell(0, 0, dto.Line15Desc)
-	// pdf.SetXY(136, 145)
-	// if dto.Line15Price == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line15Price)
-	// }
-	// pdf.SetXY(169, 145)
-	// if dto.Line15Amount == "0" {
-	// 	pdf.Cell(0, 0, "")
-	// } else {
-	// 	pdf.Cell(0, 0, dto.Line15Amount)
-	// }
-	//
-	// //
-	// // Footer
-	// //
-	//
-	// pdf.SetXY(66, 155)
-	// pdf.Cell(0, 0, dto.InvoiceQuoteDays)
-	// pdf.SetXY(146, 151)
-	// pdf.Cell(0, 0, dto.TotalLabour)
-	// pdf.SetXY(146, 160)
-	// pdf.Cell(0, 0, dto.TotalMaterials)
-	// pdf.SetXY(146, 167)
-	// pdf.Cell(0, 0, dto.OtherCosts)
-	// pdf.SetXY(146, 175)
-	// pdf.Cell(0, 0, dto.SubTotal)
-	// pdf.SetXY(146, 182)
-	// pdf.Cell(0, 0, dto.Tax)
-	// pdf.SetXY(146, 192)
-	// pdf.Cell(0, 0, dto.Total)
-	// pdf.SetXY(77, 170)
-	// pdf.Cell(0, 0, dto.InvoiceAssociateTax)
-	// pdf.SetXY(77, 179)
-	// pdf.Cell(0, 0, dto.InvoiceQuoteDate)
-	// pdf.SetXY(77, 187)
-	// pdf.Cell(0, 0, dto.InvoiceCustomersApproval)
-	// pdf.SetXY(19, 206)
-	// pdf.Cell(0, 0, dto.Line01Notes)
-	// pdf.SetXY(19, 216)
-	// pdf.Cell(0, 0, dto.Line02Notes)
-	// pdf.SetXY(55, 225)
-	// pdf.Cell(0, 0, dto.PaymentDate)
-	// pdf.SetXY(158, 225)
-	// pdf.Cell(0, 0, dto.PaymentAmount)
-	// pdf.SetXY(158, 217)
-	// pdf.Cell(0, 0, dto.Deposit)
-	//
-	// //
-	// // Checks
-	// //
-	//
-	// pdf.SetXY(27, 232)
-	// pdf.Cell(0, 0, dto.IsCash)
-	// pdf.SetXY(62, 232)
-	// pdf.Cell(0, 0, dto.IsCheque)
-	// pdf.SetXY(97, 232)
-	// pdf.Cell(0, 0, dto.IsDebit)
-	// pdf.SetXY(131, 232)
-	// pdf.Cell(0, 0, dto.IsCredit)
-	// pdf.SetXY(167, 232)
-	// pdf.Cell(0, 0, dto.IsOther)
-	// pdf.SetXY(42, 248)
-	// pdf.Cell(0, 0, dto.AssociateSignDate)
-	// pdf.SetXY(155, 248)
-	// pdf.Cell(0, 0, dto.AssociateSignature)
-	// pdf.SetXY(170, 262)
-	// pdf.Cell(0, 0, dto.WorkOrderId)
-	//
-	// //
-	// // Signature
-	// //
-	//
-	// pdf.SetXY(128, 239)
-	// pdf.Cell(0, 0, dto.ClientSignature)
+
+	pdf.SetFont("Helvetica", "B", 14) // This controls the next text.
+
+	// ROW 1
+	pdf.SetXY(92, 75)
+	pdf.Cell(0, 0, "PR")
+	pdf.SetXY(110, 75)
+	pdf.Cell(0, 0, "FR")
+	pdf.SetXY(127, 75)
+	pdf.Cell(0, 0, "GD")
+	pdf.SetXY(144, 75)
+	pdf.Cell(0, 0, "VG")
+	pdf.SetXY(163, 75)
+	pdf.Cell(0, 0, "FN")
+	pdf.SetXY(180, 75)
+	pdf.Cell(0, 0, "VF")
+	pdf.SetXY(197, 75)
+	pdf.Cell(0, 0, "NM")
+
+	// ROW 2
+	pdf.SetXY(92, 83)
+	pdf.Cell(0, 0, "PR")
+	pdf.SetXY(110, 83)
+	pdf.Cell(0, 0, "FR")
+	pdf.SetXY(127, 83)
+	pdf.Cell(0, 0, "GD")
+	pdf.SetXY(144, 83)
+	pdf.Cell(0, 0, "VG")
+	pdf.SetXY(163, 83)
+	pdf.Cell(0, 0, "FN")
+	pdf.SetXY(180, 83)
+	pdf.Cell(0, 0, "VF")
+	pdf.SetXY(197, 83)
+	pdf.Cell(0, 0, "NM")
+
+	// ROW 3
+	pdf.SetXY(92, 91)
+	pdf.Cell(0, 0, "PR")
+	pdf.SetXY(110, 91)
+	pdf.Cell(0, 0, "FR")
+	pdf.SetXY(127, 91)
+	pdf.Cell(0, 0, "GD")
+	pdf.SetXY(144, 91)
+	pdf.Cell(0, 0, "VG")
+	pdf.SetXY(163, 91)
+	pdf.Cell(0, 0, "FN")
+	pdf.SetXY(180, 91)
+	pdf.Cell(0, 0, "VF")
+	pdf.SetXY(197, 91)
+	pdf.Cell(0, 0, "NM")
+
+	// ROW 4
+	pdf.SetXY(92, 98)
+	pdf.Cell(0, 0, "PR")
+	pdf.SetXY(110, 98)
+	pdf.Cell(0, 0, "FR")
+	pdf.SetXY(127, 98)
+	pdf.Cell(0, 0, "GD")
+	pdf.SetXY(144, 98)
+	pdf.Cell(0, 0, "VG")
+	pdf.SetXY(163, 98)
+	pdf.Cell(0, 0, "FN")
+	pdf.SetXY(180, 98)
+	pdf.Cell(0, 0, "VF")
+	pdf.SetXY(197, 98)
+	pdf.Cell(0, 0, "NM")
+
+	// ROW 5
+	pdf.SetXY(92, 106)
+	pdf.Cell(0, 0, "PR")
+	pdf.SetXY(110, 106)
+	pdf.Cell(0, 0, "FR")
+	pdf.SetXY(127, 106)
+	pdf.Cell(0, 0, "GD")
+	pdf.SetXY(144, 106)
+	pdf.Cell(0, 0, "VG")
+	pdf.SetXY(163, 106)
+	pdf.Cell(0, 0, "FN")
+	pdf.SetXY(180, 106)
+	pdf.Cell(0, 0, "VF")
+	pdf.SetXY(197, 106)
+	pdf.Cell(0, 0, "NM")
+
+	// ROW 6
+	pdf.SetXY(92, 113)
+	pdf.Cell(0, 0, "PR")
+	pdf.SetXY(110, 113)
+	pdf.Cell(0, 0, "FR")
+	pdf.SetXY(127, 113)
+	pdf.Cell(0, 0, "GD")
+	pdf.SetXY(144, 113)
+	pdf.Cell(0, 0, "VG")
+	pdf.SetXY(163, 113)
+	pdf.Cell(0, 0, "FN")
+	pdf.SetXY(180, 113)
+	pdf.Cell(0, 0, "VF")
+	pdf.SetXY(197, 113)
+	pdf.Cell(0, 0, "NM")
+
+	// ROW 7
+	pdf.SetXY(92, 121)
+	pdf.Cell(0, 0, "PR")
+	pdf.SetXY(110, 121)
+	pdf.Cell(0, 0, "FR")
+	pdf.SetXY(127, 121)
+	pdf.Cell(0, 0, "GD")
+	pdf.SetXY(144, 121)
+	pdf.Cell(0, 0, "VG")
+	pdf.SetXY(163, 121)
+	pdf.Cell(0, 0, "FN")
+	pdf.SetXY(180, 121)
+	pdf.Cell(0, 0, "VF")
+	pdf.SetXY(197, 121)
+	pdf.Cell(0, 0, "NM")
+
+	// ROW 8
+	pdf.SetXY(92, 129)
+	pdf.Cell(0, 0, "PR")
+	pdf.SetXY(110, 129)
+	pdf.Cell(0, 0, "FR")
+	pdf.SetXY(127, 129)
+	pdf.Cell(0, 0, "GD")
+	pdf.SetXY(144, 129)
+	pdf.Cell(0, 0, "VG")
+	pdf.SetXY(163, 129)
+	pdf.Cell(0, 0, "FN")
+	pdf.SetXY(180, 129)
+	pdf.Cell(0, 0, "VF")
+	pdf.SetXY(197, 129)
+	pdf.Cell(0, 0, "NM")
 
 	////
 	//// Generate the file and save it to the file.
