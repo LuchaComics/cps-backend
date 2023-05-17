e65024242a34a67857341916d0b2d8d6270ba4d3
Bartlomiej Mika <bmika@icloud.com>
2023-05-17 00:38:44 -0400

    Implemented the PDF generator

--- adapter/pdfbuilder/cbff.go
+++ adapter/pdfbuilder/cbff.go
@@ -18,46 +18,37 @@ import (
 )
 
 type CBFFBuilderRequestDTO struct {
-	ID                       primitive.ObjectID `bson:"_id" json:"id"`
-	Date                     time.Time          `bson:"date" json:"date"`
-	Item                     string             `bson:"item" json:"item"` // Created by system.
-	SeriesTitle              string             `bson:"series_title" json:"series_title"`
-	IssueVol                 string             `bson:"issue_vol" json:"issue_vol"`
-	IssueNo                  string             `bson:"issue_no" json:"issue_no"`
-	IssueCoverDate           string             `bson:"issue_cover_date" json:"issue_cover_date"`
-	PublisherName            string             `bson:"publisher_name" json:"publisher_name"`
-	IssueSpecialDetails      string             `bson:"issue_special_details" json:"issue_special_details"`
-	CreasesFinding           string             `bson:"creases_finding" json:"creases_finding"`
-	TearsFinding             string             `bson:"tears_finding" json:"tears_finding"`
-	MissingPartsFinding      string             `bson:"missing_parts_finding" json:"missing_parts_finding"`
-	StainsFinding            string             `bson:"stains_finding" json:"stains_finding"`
-	DistortionFinding        string             `bson:"distortion_finding" json:"distortion_finding"`
-	PaperQualityFinding      string             `bson:"paper_quality_finding" json:"paper_quality_finding"`
-	SpineFinding             string             `bson:"spine_finding" json:"spine_finding"`
-	CoverFinding             string             `bson:"cover_finding" json:"cover_finding"`
-	OtherFinding             string             `bson:"other_finding" json:"other_finding"`
-	OtherFindingText         string             `bson:"other_finding_text" json:"other_finding_text"`
-	OverallLetterGrade       string             `bson:"overall_letter_grade" json:"overall_letter_grade"`
-	UserID                   primitive.ObjectID `bson:"user_id" json:"user_id"`
-	UserFirstName            string             `bson:"user_first_name" json:"user_first_name"`
-	UserLastName             string             `bson:"user_last_name" json:"user_last_name"`
-	UserCompanyName          string             `bson:"user_company_name" json:"user_company_name"`
-	UserSignature            string             `bson:"user_signature" json:"user_signature"`
-	InspectorSignature       string             `bson:"inspector_signature" json:"inspector_signature"`
-	InspectorDate            time.Time          `bson:"inspector_date" json:"inspector_date"`
-	InspectorFirstName       string             `bson:"inspector_first_name" json:"inspector_first_name"`
-	InspectorLastName        string             `bson:"inspector_last_name" json:"inspector_last_name"`
-	InspectorCompany         string             `bson:"inspector_company_name" json:"inspector_company_name"`
-	SecondInspectorSignature string             `bson:"second_inspector_signature" json:"second_inspector_signature"`
-	SecondInspectorFirstName string             `bson:"second_inspector_first_name" json:"second_inspector_first_name"`
-	SecondInspectorLastName  string             `bson:"second_inspector_last_name" json:"second_inspector_last_name"`
-	SecondInspectorCompany   string             `bson:"second_inspector_company" json:"second_inspector_company"`
-	SecondInspectorDate      time.Time          `bson:"second_inspector_date" json:"second_inspector_date"`
-	ThirdInspectorSignature  string             `bson:"third_inspector_signature" json:"third_inspector_signature"`
-	ThirdInspectorFirstName  string             `bson:"third_inspector_first_name" json:"third_inspector_first_name"`
-	ThirdInspectorLastName   string             `bson:"third_inspector_last_name" json:"third_inspector_last_name"`
-	ThirdInspectorCompany    string             `bson:"third_inspector_company" json:"third_inspector_company"`
-	ThirdInspectorDate       time.Time          `bson:"third_inspector_date" json:"third_inspector_date"`
+	ID                                 primitive.ObjectID `bson:"_id" json:"id"`
+	Date                               time.Time          `bson:"date" json:"date"`
+	Item                               string             `bson:"item" json:"item"` // Created by system.
+	SeriesTitle                        string             `bson:"series_title" json:"series_title"`
+	IssueVol                           string             `bson:"issue_vol" json:"issue_vol"`
+	IssueNo                            string             `bson:"issue_no" json:"issue_no"`
+	IssueCoverDate                     string             `bson:"issue_cover_date" json:"issue_cover_date"`
+	PublisherName                      string             `bson:"publisher_name" json:"publisher_name"`
+	SpecialNotesLine1                  string             `bson:"special_notes_line_1" json:"special_notes_line_1"`
+	SpecialNotesLine2                  string             `bson:"special_notes_line_2" json:"special_notes_line_2"`
+	SpecialNotesLine3                  string             `bson:"special_notes_line_3" json:"special_notes_line_3"`
+	SpecialNotesLine4                  string             `bson:"special_notes_line_4" json:"special_notes_line_4"`
+	SpecialNotesLine5                  string             `bson:"special_notes_line_5" json:"special_notes_line_5"`
+	GradingNotesLine1                  string             `bson:"grading_notes_line_1" json:"grading_notes_line_1"`
+	GradingNotesLine2                  string             `bson:"grading_notes_line_2" json:"grading_notes_line_2"`
+	GradingNotesLine3                  string             `bson:"grading_notes_line_3" json:"grading_notes_line_3"`
+	GradingNotesLine4                  string             `bson:"grading_notes_line_4" json:"grading_notes_line_4"`
+	GradingNotesLine5                  string             `bson:"grading_notes_line_5" json:"grading_notes_line_5"`
+	CreasesFinding                     string             `bson:"creases_finding" json:"creases_finding"`
+	TearsFinding                       string             `bson:"tears_finding" json:"tears_finding"`
+	MissingPartsFinding                string             `bson:"missing_parts_finding" json:"missing_parts_finding"`
+	StainsFinding                      string             `bson:"stains_finding" json:"stains_finding"`
+	DistortionFinding                  string             `bson:"distortion_finding" json:"distortion_finding"`
+	PaperQualityFinding                string             `bson:"paper_quality_finding" json:"paper_quality_finding"`
+	SpineFinding                       string             `bson:"spine_finding" json:"spine_finding"`
+	CoverFinding                       string             `bson:"cover_finding" json:"cover_finding"`
+	ShowsSignsOfTamperingOrRestoration bool               `bson:"shows_signs_of_tampering_or_restoration" json:"shows_signs_of_tampering_or_restoration"`
+	OverallLetterGrade                 string             `bson:"overall_letter_grade" json:"overall_letter_grade"`
+	UserFirstName                      string             `bson:"user_first_name" json:"user_first_name"`
+	UserLastName                       string             `bson:"user_last_name" json:"user_last_name"`
+	UserCompanyName                    string             `bson:"user_company_name" json:"user_company_name"`
 }
 type CBFFBuilderResponseDTO struct {
 	FileName string `json:"file_name"`
@@ -161,143 +152,234 @@ func (bdr *cbffBuilder) GeneratePDF(r *CBFFBuilderRequestDTO) (*CBFFBuilderRespo
 
 	pdf.SetFont("Helvetica", "B", 14) // This controls the next text.
 
-	// ROW 1
-	pdf.SetXY(92, 75)
-	pdf.Cell(0, 0, "PR")
-	pdf.SetXY(110, 75)
-	pdf.Cell(0, 0, "FR")
-	pdf.SetXY(127, 75)
-	pdf.Cell(0, 0, "GD")
-	pdf.SetXY(144, 75)
-	pdf.Cell(0, 0, "VG")
-	pdf.SetXY(163, 75)
-	pdf.Cell(0, 0, "FN")
-	pdf.SetXY(180, 75)
-	pdf.Cell(0, 0, "VF")
-	pdf.SetXY(197, 75)
-	pdf.Cell(0, 0, "NM")
+	// ROW 1 - Creases
+	switch r.CreasesFinding {
+	case "PR":
+		pdf.SetXY(92, 75)
+		pdf.Cell(0, 0, "PR")
+	case "FR":
+		pdf.SetXY(110, 75)
+		pdf.Cell(0, 0, "FR")
+	case "GD":
+		pdf.SetXY(127, 75)
+		pdf.Cell(0, 0, "GD")
+	case "VG":
+		pdf.SetXY(144, 75)
+		pdf.Cell(0, 0, "VG")
+	case "FN":
+		pdf.SetXY(163, 75)
+		pdf.Cell(0, 0, "FN")
+	case "VF":
+		pdf.SetXY(180, 75)
+		pdf.Cell(0, 0, "VF")
+	case "NM":
+		pdf.SetXY(197, 75)
+		pdf.Cell(0, 0, "NM")
+	default:
+		return nil, errors.New("missing value for crease finding")
+	}
 
-	// ROW 2
-	pdf.SetXY(92, 83)
-	pdf.Cell(0, 0, "PR")
-	pdf.SetXY(110, 83)
-	pdf.Cell(0, 0, "FR")
-	pdf.SetXY(127, 83)
-	pdf.Cell(0, 0, "GD")
-	pdf.SetXY(144, 83)
-	pdf.Cell(0, 0, "VG")
-	pdf.SetXY(163, 83)
-	pdf.Cell(0, 0, "FN")
-	pdf.SetXY(180, 83)
-	pdf.Cell(0, 0, "VF")
-	pdf.SetXY(197, 83)
-	pdf.Cell(0, 0, "NM")
+	// ROW 2 - Tears
+	switch r.TearsFinding {
+	case "PR":
+		pdf.SetXY(92, 83)
+		pdf.Cell(0, 0, "PR")
+	case "FR":
+		pdf.SetXY(110, 83)
+		pdf.Cell(0, 0, "FR")
+	case "GD":
+		pdf.SetXY(127, 83)
+		pdf.Cell(0, 0, "GD")
+	case "VG":
+		pdf.SetXY(144, 83)
+		pdf.Cell(0, 0, "VG")
+	case "FN":
+		pdf.SetXY(163, 83)
+		pdf.Cell(0, 0, "FN")
+	case "VF":
+		pdf.SetXY(180, 83)
+		pdf.Cell(0, 0, "VF")
+	case "NM":
+		pdf.SetXY(197, 83)
+		pdf.Cell(0, 0, "NM")
+	default:
+		return nil, errors.New("missing value for tears finding")
+	}
 
-	// ROW 3
-	pdf.SetXY(92, 91)
-	pdf.Cell(0, 0, "PR")
-	pdf.SetXY(110, 91)
-	pdf.Cell(0, 0, "FR")
-	pdf.SetXY(127, 91)
-	pdf.Cell(0, 0, "GD")
-	pdf.SetXY(144, 91)
-	pdf.Cell(0, 0, "VG")
-	pdf.SetXY(163, 91)
-	pdf.Cell(0, 0, "FN")
-	pdf.SetXY(180, 91)
-	pdf.Cell(0, 0, "VF")
-	pdf.SetXY(197, 91)
-	pdf.Cell(0, 0, "NM")
+	// ROW 3 - Missing Parts
+	switch r.MissingPartsFinding {
+	case "PR":
+		pdf.SetXY(92, 91)
+		pdf.Cell(0, 0, "PR")
+	case "FR":
+		pdf.SetXY(110, 91)
+		pdf.Cell(0, 0, "FR")
+	case "GD":
+		pdf.SetXY(127, 91)
+		pdf.Cell(0, 0, "GD")
+	case "VG":
+		pdf.SetXY(144, 91)
+		pdf.Cell(0, 0, "VG")
+	case "FN":
+		pdf.SetXY(163, 91)
+		pdf.Cell(0, 0, "FN")
+	case "VF":
+		pdf.SetXY(180, 91)
+		pdf.Cell(0, 0, "VF")
+	case "NM":
+		pdf.SetXY(197, 91)
+		pdf.Cell(0, 0, "NM")
+	default:
+		return nil, errors.New("missing value for missing parts finding")
+	}
 
-	// ROW 4
-	pdf.SetXY(92, 98)
-	pdf.Cell(0, 0, "PR")
-	pdf.SetXY(110, 98)
-	pdf.Cell(0, 0, "FR")
-	pdf.SetXY(127, 98)
-	pdf.Cell(0, 0, "GD")
-	pdf.SetXY(144, 98)
-	pdf.Cell(0, 0, "VG")
-	pdf.SetXY(163, 98)
-	pdf.Cell(0, 0, "FN")
-	pdf.SetXY(180, 98)
-	pdf.Cell(0, 0, "VF")
-	pdf.SetXY(197, 98)
-	pdf.Cell(0, 0, "NM")
+	// ROW 4 - Stains / Marks / Substances
+	switch r.StainsFinding {
+	case "PR":
+		pdf.SetXY(92, 98)
+		pdf.Cell(0, 0, "PR")
+	case "FR":
+		pdf.SetXY(110, 98)
+		pdf.Cell(0, 0, "FR")
+	case "GD":
+		pdf.SetXY(127, 98)
+		pdf.Cell(0, 0, "GD")
+	case "VG":
+		pdf.SetXY(144, 98)
+		pdf.Cell(0, 0, "VG")
+	case "FN":
+		pdf.SetXY(163, 98)
+		pdf.Cell(0, 0, "FN")
+	case "VF":
+		pdf.SetXY(180, 98)
+		pdf.Cell(0, 0, "VF")
+	case "NM":
+		pdf.SetXY(197, 98)
+		pdf.Cell(0, 0, "NM")
+	default:
+		return nil, errors.New("missing value for stains finding")
+	}
 
-	// ROW 5
-	pdf.SetXY(92, 106)
-	pdf.Cell(0, 0, "PR")
-	pdf.SetXY(110, 106)
-	pdf.Cell(0, 0, "FR")
-	pdf.SetXY(127, 106)
-	pdf.Cell(0, 0, "GD")
-	pdf.SetXY(144, 106)
-	pdf.Cell(0, 0, "VG")
-	pdf.SetXY(163, 106)
-	pdf.Cell(0, 0, "FN")
-	pdf.SetXY(180, 106)
-	pdf.Cell(0, 0, "VF")
-	pdf.SetXY(197, 106)
-	pdf.Cell(0, 0, "NM")
+	// ROW 5 - Distortion / Colour
+	switch r.DistortionFinding {
+	case "PR":
+		pdf.SetXY(92, 106)
+		pdf.Cell(0, 0, "PR")
+	case "FR":
+		pdf.SetXY(110, 106)
+		pdf.Cell(0, 0, "FR")
+	case "GD":
+		pdf.SetXY(127, 106)
+		pdf.Cell(0, 0, "GD")
+	case "VG":
+		pdf.SetXY(144, 106)
+		pdf.Cell(0, 0, "VG")
+	case "FN":
+		pdf.SetXY(163, 106)
+		pdf.Cell(0, 0, "FN")
+	case "VF":
+		pdf.SetXY(180, 106)
+		pdf.Cell(0, 0, "VF")
+	case "NM":
+		pdf.SetXY(197, 106)
+		pdf.Cell(0, 0, "NM")
+	default:
+		return nil, errors.New("missing value for distorion finding")
+	}
 
-	// ROW 6
-	pdf.SetXY(92, 113)
-	pdf.Cell(0, 0, "PR")
-	pdf.SetXY(110, 113)
-	pdf.Cell(0, 0, "FR")
-	pdf.SetXY(127, 113)
-	pdf.Cell(0, 0, "GD")
-	pdf.SetXY(144, 113)
-	pdf.Cell(0, 0, "VG")
-	pdf.SetXY(163, 113)
-	pdf.Cell(0, 0, "FN")
-	pdf.SetXY(180, 113)
-	pdf.Cell(0, 0, "VF")
-	pdf.SetXY(197, 113)
-	pdf.Cell(0, 0, "NM")
+	// ROW 6 - Paper Quality
+	switch r.PaperQualityFinding {
+	case "PR":
+		pdf.SetXY(92, 113)
+		pdf.Cell(0, 0, "PR")
+	case "FR":
+		pdf.SetXY(110, 113)
+		pdf.Cell(0, 0, "FR")
+	case "GD":
+		pdf.SetXY(127, 113)
+		pdf.Cell(0, 0, "GD")
+	case "VG":
+		pdf.SetXY(144, 113)
+		pdf.Cell(0, 0, "VG")
+	case "FN":
+		pdf.SetXY(163, 113)
+		pdf.Cell(0, 0, "FN")
+	case "VF":
+		pdf.SetXY(180, 113)
+		pdf.Cell(0, 0, "VF")
+	case "NM":
+		pdf.SetXY(197, 113)
+		pdf.Cell(0, 0, "NM")
+	default:
+		return nil, errors.New("missing value for paper quality finding")
+	}
 
-	// ROW 7
-	pdf.SetXY(92, 121)
-	pdf.Cell(0, 0, "PR")
-	pdf.SetXY(110, 121)
-	pdf.Cell(0, 0, "FR")
-	pdf.SetXY(127, 121)
-	pdf.Cell(0, 0, "GD")
-	pdf.SetXY(144, 121)
-	pdf.Cell(0, 0, "VG")
-	pdf.SetXY(163, 121)
-	pdf.Cell(0, 0, "FN")
-	pdf.SetXY(180, 121)
-	pdf.Cell(0, 0, "VF")
-	pdf.SetXY(197, 121)
-	pdf.Cell(0, 0, "NM")
+	// ROW 7 - Spine / Staples
+	switch r.SpineFinding {
+	case "PR":
+		pdf.SetXY(92, 121)
+		pdf.Cell(0, 0, "PR")
+	case "FR":
+		pdf.SetXY(110, 121)
+		pdf.Cell(0, 0, "FR")
+	case "GD":
+		pdf.SetXY(127, 121)
+		pdf.Cell(0, 0, "GD")
+	case "VG":
+		pdf.SetXY(144, 121)
+		pdf.Cell(0, 0, "VG")
+	case "FN":
+		pdf.SetXY(163, 121)
+		pdf.Cell(0, 0, "FN")
+	case "VF":
+		pdf.SetXY(180, 121)
+		pdf.Cell(0, 0, "VF")
+	case "NM":
+		pdf.SetXY(197, 121)
+		pdf.Cell(0, 0, "NM")
+	default:
+		return nil, errors.New("missing value for paper quality finding")
+	}
 
-	// ROW 8
-	pdf.SetXY(92, 129)
-	pdf.Cell(0, 0, "PR")
-	pdf.SetXY(110, 129)
-	pdf.Cell(0, 0, "FR")
-	pdf.SetXY(127, 129)
-	pdf.Cell(0, 0, "GD")
-	pdf.SetXY(144, 129)
-	pdf.Cell(0, 0, "VG")
-	pdf.SetXY(163, 129)
-	pdf.Cell(0, 0, "FN")
-	pdf.SetXY(180, 129)
-	pdf.Cell(0, 0, "VF")
-	pdf.SetXY(197, 129)
-	pdf.Cell(0, 0, "NM")
+	// ROW 8 - Cover (Front & Back)
+	switch r.CoverFinding {
+	case "PR":
+		pdf.SetXY(92, 129)
+		pdf.Cell(0, 0, "PR")
+	case "FR":
+		pdf.SetXY(110, 129)
+		pdf.Cell(0, 0, "FR")
+	case "GD":
+		pdf.SetXY(127, 129)
+		pdf.Cell(0, 0, "GD")
+	case "VG":
+		pdf.SetXY(144, 129)
+		pdf.Cell(0, 0, "VG")
+	case "FN":
+		pdf.SetXY(163, 129)
+		pdf.Cell(0, 0, "FN")
+	case "VF":
+		pdf.SetXY(180, 129)
+		pdf.Cell(0, 0, "VF")
+	case "NM":
+		pdf.SetXY(197, 129)
+		pdf.Cell(0, 0, "NM")
+	default:
+		return nil, errors.New("missing value cover finding")
+	}
 
-	// ROW 9
-	pdf.SetXY(86, 136.5)
-	pdf.Cell(0, 0, "X")
-	pdf.SetXY(101, 136.5)
-	pdf.Cell(0, 0, "X")
+	// ROW 9 - Shows signs of temp
+	if r.ShowsSignsOfTamperingOrRestoration == true {
+		pdf.SetXY(86, 136.5)
+		pdf.Cell(0, 0, "X")
+	} else {
+		pdf.SetXY(101, 136.5)
+		pdf.Cell(0, 0, "X")
+	}
 
 	// ROW 10 - Grading
 	pdf.SetXY(176, 153)
-	pdf.Cell(0, 0, "XX")
+	pdf.Cell(0, 0, r.OverallLetterGrade)
 
 	//
 	// LEFT
@@ -305,43 +387,43 @@ func (bdr *cbffBuilder) GeneratePDF(r *CBFFBuilderRequestDTO) (*CBFFBuilderRespo
 
 	// ROW 1 - Special Notes
 	pdf.SetXY(217, 75)
-	pdf.Cell(0, 0, "XXXXXXXXXXXXXXXXX") // 17 characters.
+	pdf.Cell(0, 0, r.SpecialNotesLine1) // 17 characters.
 
 	// ROW 2 - Special Notes
 	pdf.SetXY(217, 83)
-	pdf.Cell(0, 0, "XXXXXXXXXXXXXXXXX") // 17 characters.
+	pdf.Cell(0, 0, r.SpecialNotesLine2) // 17 characters.
 
 	// ROW 3 - Special Notes
 	pdf.SetXY(217, 91)
-	pdf.Cell(0, 0, "XXXXXXXXXXXXXXXXX") // 17 characters.
+	pdf.Cell(0, 0, r.SpecialNotesLine3) // 17 characters.
 
 	// ROW 4 - Special Notes
 	pdf.SetXY(217, 99)
-	pdf.Cell(0, 0, "XXXXXXXXXXXXXXXXX") // 17 characters.
+	pdf.Cell(0, 0, r.SpecialNotesLine4) // 17 characters.
 
 	// ROW 5 - Special Notes
 	pdf.SetXY(217, 107)
-	pdf.Cell(0, 0, "XXXXXXXXXXXXXXXXX") // 17 characters.
+	pdf.Cell(0, 0, r.SpecialNotesLine5) // 17 characters.
 
 	// ROW 1 - Grading Notes
 	pdf.SetXY(217, 124)
-	pdf.Cell(0, 0, "XXXXXXXXXXXXXXXXX") // 17 characters.
+	pdf.Cell(0, 0, r.GradingNotesLine1) // 17 characters.
 
 	// ROW 2 - Grading Notes
 	pdf.SetXY(217, 132)
-	pdf.Cell(0, 0, "XXXXXXXXXXXXXXXXX") // 17 characters.
+	pdf.Cell(0, 0, r.GradingNotesLine2) // 17 characters.
 
 	// ROW 3 - Grading Notes
 	pdf.SetXY(217, 140)
-	pdf.Cell(0, 0, "XXXXXXXXXXXXXXXXX") // 17 characters.
+	pdf.Cell(0, 0, r.GradingNotesLine3) // 17 characters.
 
 	// ROW 4 - Grading Notes
 	pdf.SetXY(217, 148)
-	pdf.Cell(0, 0, "XXXXXXXXXXXXXXXXX") // 17 characters.
+	pdf.Cell(0, 0, r.GradingNotesLine4) // 17 characters.
 
 	// ROW 5 - Grading Notes
 	pdf.SetXY(217, 156)
-	pdf.Cell(0, 0, "XXXXXXXXXXXXXXXXX") // 17 characters.
+	pdf.Cell(0, 0, r.GradingNotesLine5) // 17 characters.
 
 	////
 	//// Generate the file and save it to the file.
